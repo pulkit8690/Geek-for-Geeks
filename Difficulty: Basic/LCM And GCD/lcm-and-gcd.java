6fef3d5e544c011c91cfafae4831ad3114e4ce71
Pulkit Arora <103959073+pulkit8690@users.noreply.github.com>
2024-06-24 18:22:23 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/LCM And GCD/lcm-and-gcd.java
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String S[] = read.readLine().split(" ");
+            Long A = Long.parseLong(S[0]);
+            Long B = Long.parseLong(S[1]);
+
+            Solution ob = new Solution();
+            Long[] ptr = ob.lcmAndGcd(A,B);
+            
+            System.out.print(ptr[0]);
+            System.out.print(" ");
+            System.out.println(ptr[1]);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    static Long[] lcmAndGcd(Long A , Long B) {
+        Long[] arr = new Long[2];
+        arr[1] = gcd(A, B);
+        arr[0] = lcm(A, B, arr[1]);
+        return arr;
+    }
+    
+    public static long gcd(long a, long b) {
+        while (b != 0) {
+            long temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+    
+    public static long lcm(long a, long b, long gcd) {
+        return (a / gcd) * b;
+    }
+}
