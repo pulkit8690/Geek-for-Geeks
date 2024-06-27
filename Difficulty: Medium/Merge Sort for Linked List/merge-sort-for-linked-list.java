0799fe488b8a7823176e004b03ba10659e06274a
Pulkit Arora <103959073+pulkit8690@users.noreply.github.com>
2024-06-27 18:51:06 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Merge Sort for Linked List/merge-sort-for-linked-list.java
@@ -0,0 +1,138 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Node
+{
+    int data;
+    Node next;
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+
+class Driverclass
+{
+    
+    public static void main (String[] args) 
+    {
+        Scanner sc= new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            int n = sc.nextInt();
+            Node head = new Node(sc.nextInt());
+            Node tail = head;
+            while(n-- > 1){
+		        tail.next = new Node(sc.nextInt());
+		        tail = tail.next;
+		    }
+		   
+		      head = new Solution().mergeSort(head);
+		     printList(head);
+		    System.out.println();
+        }
+    }
+    public static void printList(Node head)
+    {
+        if(head == null)
+           return;
+           
+        Node temp = head;
+        while(temp != null)
+        {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+    }
+}
+
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+/*
+class Node
+{
+    int data;
+    Node next;
+    Node(int key)
+    {
+        this.data = key;
+        next = null;
+    }
+} */
+
+class Solution
+{
+    //Function to sort the given linked list using Merge Sort.
+    static Node mergeSort(Node head)
+    {
+        if(head==null || head.next==null)
+        {
+            return head;
+        }
+        // add your code here
+        //Step 1 Find Mid
+        Node midNode=findMid(head);
+        Node rightHead= midNode.next;
+        midNode.next = null;
+        Node left=mergeSort(head);
+        Node right=mergeSort(rightHead);
+        return merge(left,right);
+    }
+    static Node findMid(Node head)
+    {
+        Node slow=head;
+        Node fast=head.next;
+        while(fast!=null && fast.next!=null)
+        {
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        return slow;
+    }
+    static Node merge(Node head1, Node head2)
+    {
+        Node merge_ll= new Node(-1);
+        Node temp=merge_ll;
+        while(head1!=null && head2!=null)
+        {
+            if(head1.data<head2.data)
+            {
+                temp.next=head1;
+                head1=head1.next;
+                temp=temp.next;
+            }
+            else
+            {
+                temp.next=head2;
+                head2=head2.next;
+                temp=temp.next;
+            }
+        }
+        while(head1!=null)
+        {
+            temp.next=head1;
+            head1=head1.next;
+            temp=temp.next;
+        }
+        while(head2!=null)
+        {
+            temp.next=head2;
+            head2=head2.next;
+            temp=temp.next;
+        }
+        return merge_ll.next;
+    }
+}
+
+
