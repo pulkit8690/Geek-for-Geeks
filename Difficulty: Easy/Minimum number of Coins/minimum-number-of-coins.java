dc57a13268b0309e9e5bd10f17cc92e46e3b9dbb
Pulkit Arora <103959073+pulkit8690@users.noreply.github.com>
2024-07-10 11:13:28 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Minimum number of Coins/minimum-number-of-coins.java
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(in.readLine());
+        while(t-- > 0){
+            int N = Integer.parseInt(in.readLine());
+            
+            Solution ob = new Solution();
+            List<Integer> numbers= new ArrayList<Integer>();
+            numbers = ob.minPartition(N);
+            for(int i: numbers){
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution{
+    static List<Integer> minPartition(int N)
+    {
+        // code here
+        Integer coins[]= { 1, 2, 5, 10, 20, 50, 100, 200, 500, 2000 };
+        Arrays.sort(coins,Comparator.reverseOrder());
+        List<Integer> ans = new ArrayList<Integer>();
+        for(int i=0;i<coins.length;i++)
+        {
+            if(N>=coins[i])
+            {
+                while(N>=coins[i])
+                {
+                    ans.add(coins[i]);
+                    N=N-coins[i];
+                }
+            }
+        }
+        return ans;
+        
+    }
+}
